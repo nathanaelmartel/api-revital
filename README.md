566b4eb856209398d436e6c2a2460bd2b5e29d65
Nathanaël Martel <nat@nathanaelmartel.net>
2018-10-02 11:37:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # api-revital
 Wraper pour l'API revital assurance et social assur
+
+
+## How to use: 
+''
+require_once('_api.php');
+$api = new RetitalWrapper(array('key' => $key, 'url' => 'https://revital.adsolue.com/api'));
+$api->createProspect($supplier_id, $assurance_type = 'auto', $data, $utm_campaign);
+''
+
+* $supplier_id is the utm_source used to identify the supplier
+* $assurance_type could be anything : 'auto', 'habitation'…
+* $data is an array containing all user informations fields
+* $data **need** to contain those keys: 'firstname', 'lastname', 'tel' and 'email'
+* $utm_campaign is optionnal
+
+## Response
+
+If you have no key, or the key is wrong or disable you will get :
+''error 401: Unauthorized, you need to set an authorization key'' with a 401 http header
+
+If one of the field 'firstname', 'lastname', 'tel' or 'email' is missing in the $data array, your will get :
+''error''
+
+Else, you will have ''success'', that means we create the prospect on our side.
---
+++ RetitalWrapper.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ * How to use: 
+ *     require_once('_api.php');
+ *     $api = new RetitalWrapper(array('key' => $key, 'url' => 'https://revital.adsolue.com/api'));
+ *     $api->createProspect($supplier_id, $assurance_type = 'auto', $data);
+ *
+ * $supplier_id is the utm_source used to identify the supplier
+ * $assurance_type could be anything : 'auto', 'habitation'…
+ * $data is an array that should contain those keys: 'firstname', 'lastname', 'tel' (or 'phone', 'tel_1', 'tel_2'), 'email' (or 'mail')… and all other usefull fields
+ */
+
+class RetitalWrapper {
+
+    var $url;
+    var $key;
+
+    public function __construct($params)
+    {
+        if (isset($params['key'])) {
+            $this->key = $params['key'];
+        }
+        $this->url = 'https://revital.adsolue.com/api';
+        if (isset($params['url'])) {
+            $this->url = $params['url'];
+        }
+    }
+
+    public function getServices()
+    {
+        return $this->call('/');
+    }
+
+    public function createProspect($utm_source, $type, $datas, $utm_campaign = '')
+    {
+        $post['ip'] = $_SERVER['REMOTE_ADDR'];
+        $post['origin'] = $_SERVER['SERVER_NAME'];
+        $post['utm_source'] = $utm_source;
+        $post['utm_campaign'] = $utm_campaign;
+        $post['type'] = $type;
+        $post['form_datas'] = json_encode($datas);
+
+        return $this->call('/prospect/new', $post);
+    }
+
+    public function call($service, $post)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(sprintf('Authorization: Bearer %s', $this->key)));
+        curl_setopt($ch, CURLOPT_URL, $this->url.$service);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, $_SERVER['REQUEST_URI']);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+        /*curl_setopt($ch, CURLOPT_VERBOSE, true);
+        $verbose = fopen('php://temp', 'w+');
+        curl_setopt($ch, CURLOPT_STDERR, $verbose);*/
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+
+        /*$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($http_code == 200) {
+            return json_decode($result, true);
+        }*/
+
+        /*rewind($verbose);
+        $verboseLog = stream_get_contents($verbose);
+        var_dump($verboseLog);*/
+
+        /*return json_encode(array(
+            'error' => $http_code,
+        ));*/
+    }
+}
